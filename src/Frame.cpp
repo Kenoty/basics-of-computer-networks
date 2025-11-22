0560dcf41d40433b21babd07653ba13e76fb2ce0
Vitali Knyrevich <knyrevichvitali@gmail.com>
2025-11-22 17:44:20 +0300

    fcs support added

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,4 +1,5 @@
 #include "Frame.h"
+#include "ErrorSimulator.h"
 
 void Frame::setData(const std::vector<uint8_t>&data) {
     this->data = data;
@@ -21,6 +22,8 @@ std::vector<uint8_t> Frame::serialize() const {
 
     bytes.insert(bytes.end(), data.begin(), data.end());
 
+    bytes.insert(bytes.end(), fcs.begin(), fcs.end());
+
     bytes.push_back(this->endFlag);
 
     return bytes;
@@ -28,7 +31,7 @@ std::vector<uint8_t> Frame::serialize() const {
 
 bool Frame::deserialize(const std::vector<uint8_t>& data) {
 
-    if(data.size() < HEADER_SIZE + 2) {
+    if(data.size() < HEADER_SIZE + 1 + fcs.size() + TRAILER_SIZE) {
         return false;
     }
 
@@ -36,13 +39,31 @@ bool Frame::deserialize(const std::vector<uint8_t>& data) {
         return false;
     }
 
-
     this->startFlag = data[0];
     this->total = data[1];
     this->sequence = data[2];
 
-    this->data.assign(data.begin() + HEADER_SIZE, data.end() - 1);
+    size_t dataEndPos;
+
+    if (data.size() > 20) {
+        dataEndPos = data.size() - 1 - 2; // fcs битов 9 чтобы 2^7 (от 0 до 7) было больше чем 16 * 8 + 1 плюс 1 бит secded
+    }
+    else {
+        dataEndPos = data.size() - 1 - 1;
+    }
+
+    this->data.assign(data.begin() + HEADER_SIZE, data.begin() + dataEndPos);
+
+    this->fcs.assign(data.begin() + dataEndPos, data.end() - 1);
 
     this->endFlag = data[data.size() - 1];
     return true;
+}
+
+int Frame::correctErrors() {
+    return HammingEncoder::correctErrors(this->data, this->fcs);
+}
+
+int Frame::simulateErrors() {
+    return ErrorSimulator::simulateErrors(this->data);
 }
--- src/Frame.h
+++ src/Frame.h
@@ -4,23 +4,28 @@
 #include <vector>
 #include <string>
 
+#include "HammingEncoder.h"
+
 #define START_FLAG_BYTE 0x0B
 #define HEADER_SIZE 3
+#define TRAILER_SIZE 1
 #define END_FLAG_BYTE 0x0C
 #define ESCAPE_BYTE 0x7D
 #define XOR_MASK 0x50
 
 class Frame {
 public:
-    Frame(): startFlag(0), total(0), sequence(0), data(0), endFlag(0) {};
+    Frame(): startFlag(0), total(0), sequence(0), data(0), fcs(0), endFlag(0) {};
 
     Frame(uint8_t sequence, uint8_t total, const std::vector<uint8_t>& data)
         : startFlag(START_FLAG_BYTE), sequence(sequence), total(total), data(data), endFlag(END_FLAG_BYTE) {
+        fcs = HammingEncoder::calculateControlBits(this->data);
     }
 
     Frame(uint8_t sequence, uint8_t total, const std::string& data)
         : startFlag(START_FLAG_BYTE), sequence(sequence), total(total), endFlag(END_FLAG_BYTE) {
         setData(data);
+        fcs = HammingEncoder::calculateControlBits(this->data);
     }
 
     std::vector<uint8_t> serialize() const;
@@ -30,6 +35,7 @@ public:
     uint8_t getTotal() const { return total; }
     uint8_t getSequence() const { return sequence; }
     const std::vector<uint8_t>& getData() const { return data; }
+    const std::vector<uint8_t>& getFcs() const { return fcs; }
     uint8_t getEndFlag() const { return endFlag; }
 
     void setStartFlag(uint8_t flag) { this->startFlag = flag; }
@@ -37,14 +43,19 @@ public:
     void setSequence(uint8_t sequence) { this->sequence = sequence; }
     void setData(const std::vector<uint8_t>& data);
     void setData(const std::string& data);
+    void setFcs(std::vector<uint8_t> fcs) { this->fcs = fcs; }
     void setEndFlag(uint8_t flag) { this->endFlag = flag; }
 
     std::string dataToString() const;
+
+    int correctErrors();
+    int simulateErrors();
 
 private:
     uint8_t startFlag;
     uint8_t total;
     uint8_t sequence;
     std::vector<uint8_t> data;
+    std::vector<uint8_t> fcs;
     uint8_t endFlag;
 };
